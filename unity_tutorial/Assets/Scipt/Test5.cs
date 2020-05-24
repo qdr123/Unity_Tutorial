80e551f3e7e28938a89aac5c725fb0c76566bb14
qdr123 <tmxjdydrb12@gmail.com>
2020-05-24 17:11:06 +0900

    part 6 - 카메라 완료

--- unity_tutorial/Assets/Scipt/Test5.cs
+++ unity_tutorial/Assets/Scipt/Test5.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class Test5 : MonoBehaviour
 {
-    [SerializeField]
-    private Material red_Mat;
-    [SerializeField]
-    private Material green_Mat;
-
-    private MeshRenderer mesh;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        mesh = GetComponent<MeshRenderer>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetMouseButton(0))
-        {
-            mesh.material = green_Mat;
-        }
-        else
-        {
-            mesh.material = red_Mat;
-        }
-            
-    }
+   // [SerializeField]
+   // private Material red_Mat;
+   // [SerializeField]
+   // private Material green_Mat;
+   //
+   // private MeshRenderer mesh;
+   //
+   // // Start is called before the first frame update
+   // void Start()
+   // {
+   //     mesh = GetComponent<MeshRenderer>();
+   // }
+   //
+   // // Update is called once per frame
+   // void Update()
+   // {
+   //     if(Input.GetMouseButton(0))
+   //     {
+   //         mesh.material = green_Mat;
+   //     }
+   //     else
+   //     {
+   //         mesh.material = red_Mat;
+   //     }
+   //         
+   // }
 }
