269c79fef22209041641cd8e71c4445b9f032572
qdr123 <tmxjdydrb12@gmail.com>
2020-05-24 18:07:00 +0900

    part 8  - 애니메이션  완

---
+++ unity_tutorial/Assets/Scipt/Test8.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test8 : MonoBehaviour
+{
+
+   // private Animation anim;
+   //
+   // private AnimationClip clip;
+   // 
+   // // Start is called before the first frame update
+   // void Start()
+   // {
+   //     anim = GetComponent<Animation>();
+   // }
+   //
+   // // Update is called once per frame
+   // void Update()
+   // {
+   //     if(Input.GetKeyDown(KeyCode.W))
+   //     {
+   //         //anim.Play("Cube_2");
+   //         //anim.PlayQueued("Cube_2");
+   //         //anim.Stop();
+   //         //anim.Blend("Cube_2");
+   //         anim.clip = clip;
+   //     }
+   // }
+}
