d4018b32f2833eb961589d6860c5913964c6fe95
qdr123 <tmxjdydrb12@gmail.com>
2020-05-24 16:14:41 +0900

    part4  - 컬라이더완료

---
+++ unity_tutorial/Assets/Scipt/Test4.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test4 : MonoBehaviour
+{
+
+    private BoxCollider col;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //    //if(Input.GetKeyDown(KeyCode.W))
+    //    //{
+    //    //  //Debug.Log("col.bounds"+col.bounds);
+    //    //  //Debug.Log("col.bounds.extents"+col.bounds.extents);//수정 불가
+    //    //  //Debug.Log("col.bounds.extents.x" + col.bounds.extents.x);//수정 불가
+    //    //  //Debug.Log("col.size" + col.size);
+    //    //  //Debug.Log("col.center" + col.center);
+    //    //
+    //    //    
+    //    //
+    //    //}
+    //   //if(Input.GetMouseButtonDown(0))
+    //   //  {
+    //   //      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //   //      RaycastHit hitinto;
+    //   //      if(col.Raycast(ray,out hitinto,1000))
+    //   //      {
+    //   //          this.transform.position = hitinto.point;
+    //   //      }
+    //   //  }
+    //
+    //
+    //
+    //        
+    // }
+    
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.position += new Vector3(0, 0, 0.01f);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.position = new Vector3(0, 2, 0);
+    }
+}
